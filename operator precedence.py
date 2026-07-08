a2ba33e31b643b564dadfabc2a02bec08a2c9813
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-07-08 10:28:46 -0400

    Add files via upload

---
+++ operator precedence.py
@@ -0,0 +1,18 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z= (v+w)*x/y 
+print ("Value of v + w multiplied by x/y equals", z)
+
+a=0
+a= (x+v)/(w-y)
+print ("The value of x+v divided by w-y equals", a)
+
+name="Joshitha"
+age=14
+if name=="Joshitha" or name=="Lyre" and age>=10:
+    print ("Welcome in.")
+else:
+    print ("Denied access")
