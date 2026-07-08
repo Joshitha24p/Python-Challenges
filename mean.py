88d3370a583a7e54936825a8cb14b5c9143d6b62
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-07-08 10:44:33 -0400

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,12 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_value=40
+sum=mean1*total_value
+print ("the sum is", sum)
+
+num2=(sum-wrong_number)+correct_number
+print ("The correct sum is", num2)
+
+mean= num2/total_value
+print ("The mean is", mean)
