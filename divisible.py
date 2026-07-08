2162c2492a7d74c2567c85a43b1b6886f34c75c7
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-07-08 10:45:12 -0400

    Add files via upload

---
+++ divisible.py
@@ -0,0 +1,7 @@
+numn=int(input("Insert a number(numerator)"))
+numd=int(input("Insert a number(denominator)"))
+
+if numn%numd==0:
+    print (numn, "is divisble by", numd)
+else:
+    print (numn,"is not divisble by", numd)
